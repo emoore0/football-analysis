6f9ae6c0090f306678c007e825cc44fbdd59e2c2
emoore0 <emmanuelmoore0@gmail.com>
2024-06-17 06:31:08 +0000

    change footie app

--- footie.py
+++ footie.py
@@ -1,2 +1,20 @@
+<<<<<<< HEAD
 class footie:
     
+=======
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+class footie:
+    data = 0
+
+    def __init__(self,file):
+        df = pd.read_csv(file)
+        self.data = df
+        print(self.data)
+
+
+f = footie('PL 23-24 Data.csv')
+>>>>>>> 2f200fb (start outcome method)
