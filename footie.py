2163901bbd7e933ada01a8f5855b39bb51ee6e6a
emoore0 <emmanuelmoore0@gmail.com>
2024-08-05 16:36:26 +0000

    testing if flask works

--- footie.py
+++ footie.py
@@ -155,10 +155,10 @@ app = Flask(__name__)
 f = footie('PL 23-24 Data.csv')
 @app.route('/')
 def home():
-    #report = f.the_best()
-    #return render_template_string(report)
-    graph = f.outcomes(10,"home")
-    return render_template_string(graph)
+    report = f.the_best()
+    return render_template_string(report)
+    #graph = f.outcomes(10,"home")
+    #return render_template_string(graph)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
