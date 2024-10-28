12b6baaec8dd5727f9bfc155ff64c0b9570194a6
emoore0 <emmanuelmoore0@gmail.com>
2024-10-28 18:13:18 +0000

    update footie.py again

--- footie.py
+++ footie.py
@@ -119,7 +119,7 @@ class footie:
 
         fig = Figure(figsize=(16, 8))
         ax = fig.subplots()
-        colors = ['#3498db', '#e74c3c', '#2ecc71', '#f1c40f','#9b59b6', '#34495e', '#16a085', '#e67e22','#95a5a6', '#d35400', '#c0392b', '#7f8c8d','#2c3e50', '#27ae60', '#8e44ad', '#1abc9c','#f39c12', '#bdc3c7', '#2980b9', '#e84393'][:len(Awins_teams)]
+        colors = ['#3498db', '#e74c3c', '#2ecc71', '#f1c40f','#9b59b6', '#34495e', '#16a085', '#e67e22','#95a5a6', '#d35400', '#c0392b', '#7f8c8d','#2c3e50', '#27ae60', '#8e44ad', '#1abc9c','#f39c12', '#bdc3c7', '#2980b9', '#e84393'][:len(team_who_scored)]
         ax.bar(team_who_scored,teams_values,color=colors)
         ax.set_xlabel('Teams')
         ax.set_ylabel('Matches where both teams scored')
